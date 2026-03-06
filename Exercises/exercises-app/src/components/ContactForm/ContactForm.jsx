92e4cd9871cf88233287bf53cfbd3848cecf4344
EmmaWikstrom <emma.malmwikstrom@gmail.com>
2026-03-06 12:41:12 +0100

    feat: Add contact form with name, email, and message fields

--- Exercises/exercises-app/src/components/ContactForm/ContactForm.jsx
+++ Exercises/exercises-app/src/components/ContactForm/ContactForm.jsx
@@ -4,6 +4,19 @@ const ContactForm = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+
+    return (
+            <form>
+                <label htmlFor="name">Name:</label>
+                <input id="name" type="text" name="username" placeholder='Name' />
+                <label htmlFor="email">Email:</label>
+                <input id="email" type="email" name="email" placeholder="Email" />
+                <label htmlFor="message">Message:</label>
+                <textarea id="message" name="message" placeholder="Your message" />
+                <button type="submit">Submit</button>
+            </form>
+    )
+
 };  
 
 export default ContactForm;
