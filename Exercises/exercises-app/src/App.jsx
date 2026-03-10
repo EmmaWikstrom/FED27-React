d973cad225c598b85a11a32e113c54978f3323bb
EmmaWikstrom <emma.malmwikstrom@gmail.com>
2026-03-10 10:18:35 +0100

    feat: Add form for user input with name, email, and message fields

--- Exercises/exercises-app/src/App.jsx
+++ Exercises/exercises-app/src/App.jsx
@@ -1,11 +1,20 @@
-import user from './mockUser.json';
-import Layout from './components/Layout';
-import Homepage from './components/Homepage/Homepage';
+import user from "./mockUser.json";
+import Layout from "./components/Layout";
+import Homepage from "./components/Homepage/Homepage";
 
 function App() {
   return (
     <Layout>
       <Homepage user={user} />
+      <form>
+        <label htmlFor="name">Name:</label>
+        <input id="name" type="text" name="username" placeholder="Name" />
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" name="email" placeholder="Email" />
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" name="message" placeholder="Your message" />
+        <button type="submit">Submit</button>
+      </form>
     </Layout>
   );
 }
